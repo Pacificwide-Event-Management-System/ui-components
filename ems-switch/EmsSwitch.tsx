74695946947cdeccb24bf77c9324496bb97c2704
tpthanhan <tpthanhan@gmail.com>
2024-03-18 20:59:13 +0700

    feat: Ems Switch

---
+++ ems-switch/EmsSwitch.tsx
@@ -0,0 +1,21 @@
+import { Switch, SwitchProps } from 'antd';
+import clsx from 'clsx';
+import './EmsSwitch.scss';
+
+type Props = {};
+
+// Custom components
+function EmsSwitch(props: Props & SwitchProps) {
+  return (
+    <>
+      <Switch
+        {...props}
+        className={clsx('ems-switch', props.className)}
+        checkedChildren={<div className="h-[10px] w-[1px] bg-neutral-8"></div>}
+        unCheckedChildren={<div className="h-[10px] w-[1px] bg-neutral-8"></div>}
+      ></Switch>
+    </>
+  );
+}
+
+export default EmsSwitch;
