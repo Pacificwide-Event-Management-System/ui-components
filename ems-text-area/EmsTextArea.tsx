db5f24d445e0465625547938d8eff871aedf7e7e
bumblebee2220 <hieuphamdtu2220@gmail.com>
2024-03-21 15:19:42 +0700

    Fix warning html

--- ems-text-area/EmsTextArea.tsx
+++ ems-text-area/EmsTextArea.tsx
@@ -1,6 +1,6 @@
-import { TextAreaProps } from 'antd/es/input';
-import TextArea from 'antd/es/input/TextArea';
-import clsx from 'clsx';
+import { TextAreaProps } from "antd/es/input";
+import TextArea from "antd/es/input/TextArea";
+import clsx from "clsx";
 
 type Props = {
   id?: string;
@@ -22,21 +22,23 @@ function EmsTextArea(props: Props & TextAreaProps) {
         </div>
       )}
       <TextArea
-        {...props}
+        {...omit(props, ["textInputClassName"])}
         className={clsx(
-          'block w-full rounded-lg border !px-3 !py-2 !text-base text-neutral-1 !outline-[0] hover:border-primary-2 active:border-primary-3 active:[box-shadow:none]',
-          'disabled:border-none disabled:!bg-neutral-7 disabled:!text-neutral-3 disabled:hover:border-neutral-5',
+          "block w-full rounded-lg border !px-3 !py-2 !text-base text-neutral-1 !outline-[0] hover:border-primary-2 active:border-primary-3 active:[box-shadow:none]",
+          "disabled:border-none disabled:!bg-neutral-7 disabled:!text-neutral-3 disabled:hover:border-neutral-5",
           // 'focus:shadow-[0px_0px_0px_3px_#FC50554D] focus:ring-0',
-          props.helperText ? 'border-accent-red' : 'border-neutral-5',
-          props.textInputClassName,
+          props.helperText ? "border-accent-red" : "border-neutral-5",
+          props.textInputClassName
         )}
         id={props.id}
-        autoComplete={props.autoComplete ?? 'off'}
-        color={props.helperText ? 'failure' : ''}
-        style={{ resize: props.disableResize ? 'none' : 'vertical' }}
+        autoComplete={props.autoComplete ?? "off"}
+        color={props.helperText ? "failure" : ""}
+        style={{ resize: props.disableResize ? "none" : "vertical" }}
       />
       {props.helperText && (
-        <label className="mt-2 text-sm text-accent-red">{props.helperText}</label>
+        <label className="mt-2 text-sm text-accent-red">
+          {props.helperText}
+        </label>
       )}
     </>
   );
