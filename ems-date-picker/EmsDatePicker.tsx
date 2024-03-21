ed52b25bf385d4e7d5eed26b15c3a0d094e97067
tpthanhan <tpthanhan@gmail.com>
2024-03-21 23:51:42 +0700

    style: switch and date picker

--- ems-date-picker/EmsDatePicker.tsx
+++ ems-date-picker/EmsDatePicker.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { DatePicker } from 'antd';
+import clsx from 'clsx';
 import { HTMLInputTypeAttribute } from 'react';
+import './EmsDatePicker.scss';
 
 type Props = {
   id?: string;
@@ -22,19 +24,23 @@ type Props = {
 // Custom components
 function EmsDatePicker(props: Props) {
   return (
-    <div className={props.className}>
-      <DatePicker
-        value={props.value}
-        autoComplete={props.autoComplete ?? 'off'}
-        id={props.id}
-        type={props.type}
-        placeholder={props.placeholder}
-        required={props.required}
-        onChange={props.onChange}
-        color={props.helperText ? 'failure' : ''}
-        disabled={props.disabled}
-      />
-    </div>
+    <DatePicker
+      format={'MM/DD/YYYY'}
+      allowClear={false}
+      className={clsx(
+        'flex !h-10 w-full items-center rounded-lg border border-neutral-5 px-3 !text-base text-neutral-1 !outline-[0] hover:border-primary-2',
+        'active:border-primary-3 active:[box-shadow:none]',
+        'disabled:border-none disabled:!bg-neutral-7 disabled:!text-neutral-3 disabled:hover:border-neutral-5',
+        'focus:shadow-[0px_0px_0px_3px_#FC50554D] focus:ring-0',
+        'overflow-hidden',
+      )}
+      value={props.value}
+      onChange={(value) => props.onChange(value)}
+      autoComplete={props.autoComplete ?? 'off'}
+      required={props.required}
+      placeholder={props.placeholder}
+      disabled={props.disabled}
+    />
   );
 }
 
