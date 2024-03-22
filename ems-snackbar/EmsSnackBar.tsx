0254463f07446291464537ed5b7a00ab34bd49ab
levanduc09 <levanduc094@gmail.com>
2024-03-22 13:50:41 +0700

    fix: snackbar

--- ems-snackbar/EmsSnackBar.tsx
+++ ems-snackbar/EmsSnackBar.tsx
@@ -1,12 +1,12 @@
-'use client';
-import Close from '@/static/close.svg';
-import Error from '@/static/error.svg';
-import Info from '@/static/info.svg';
-import Success from '@/static/success.svg';
-import Warning from '@/static/warning.svg';
-import Image from 'next/image';
-import { CustomContentProps, ProviderContext, SnackbarContent, useSnackbar } from 'notistack';
-import { forwardRef } from 'react';
+"use client";
+import Close from "@/static/close.svg";
+import Error from "@/static/error.svg";
+import Info from "@/static/info.svg";
+import Success from "@/static/success.svg";
+import Warning from "@/static/warning.svg";
+import Image from "next/image";
+import { CustomContentProps, ProviderContext, SnackbarContent, useSnackbar } from "notistack";
+import { forwardRef } from "react";
 
 let useSnackbarRef: ProviderContext;
 export const SnackbarUtilsConfigurator = () => {
@@ -23,94 +23,64 @@ interface EmsSnackbarProps extends CustomContentProps {
   message: string;
 }
 
-export const EmsSnackbar = forwardRef<HTMLDivElement, EmsSnackbarProps>(
-  ({ id, message, variant, ...props }, ref) => {
-    return (
-      <SnackbarContent ref={ref} className="flex items-center justify-center">
-        <div className="flex h-7 items-center gap-2 rounded-md border-[0.5px] border-neutral-4 bg-neutral-8 px-2 py-1">
-          {variant == 'success' && (
-            <>
-              <Image src={Success.src} width={16} height={16} alt="" />
-              <div className="text-sm font-normal">{message}</div>
-              <Image
-                src={Close.src}
-                width={20}
-                height={20}
-                alt=""
-                className="cursor-pointer"
-                onClick={() => onClickDismiss(id)}
-              />
-            </>
-          )}
-          {variant == 'info' && (
-            <>
-              <Image src={Info.src} width={16} height={16} alt="" />
-              <div className="text-sm font-normal">{message}</div>
-              <Image
-                src={Close.src}
-                width={20}
-                height={20}
-                alt=""
-                className="cursor-pointer"
-                onClick={() => onClickDismiss(id)}
-              />
-            </>
-          )}
-          {variant == 'warning' && (
-            <>
-              <Image src={Warning.src} width={16} height={16} alt="" />
-              <div className="text-sm font-normal">{message}</div>
-              <Image
-                src={Close.src}
-                width={20}
-                height={20}
-                alt=""
-                className="cursor-pointer"
-                onClick={() => onClickDismiss(id)}
-              />
-            </>
-          )}
-          {variant == 'error' && (
-            <>
-              <Image src={Error.src} width={16} height={16} alt="" />
-              <div className="text-sm font-normal text-accent-red">{message}</div>
-              <Image
-                src={Close.src}
-                width={20}
-                height={20}
-                alt=""
-                className="cursor-pointer"
-                onClick={() => onClickDismiss(id)}
-              />
-            </>
-          )}
-        </div>
-      </SnackbarContent>
-    );
-  },
-);
+export const EmsSnackbar = forwardRef<HTMLDivElement, EmsSnackbarProps>(({ id, message, variant, ...props }, ref) => {
+  return (
+    <SnackbarContent ref={ref} className="fixed top-[4.75rem] z-[999999] flex items-center justify-center">
+      <div className="flex h-7 items-center gap-2 rounded-md border-[0.5px] border-neutral-4 bg-neutral-8 px-2 py-1">
+        {variant == "success" && (
+          <>
+            <Image src={Success.src} width={16} height={16} alt="" />
+            <div className="text-sm font-normal">{message}</div>
+            <Image src={Close.src} width={20} height={20} alt="" className="cursor-pointer" onClick={() => onClickDismiss(id)} />
+          </>
+        )}
+        {variant == "info" && (
+          <>
+            <Image src={Info.src} width={16} height={16} alt="" />
+            <div className="text-sm font-normal">{message}</div>
+            <Image src={Close.src} width={20} height={20} alt="" className="cursor-pointer" onClick={() => onClickDismiss(id)} />
+          </>
+        )}
+        {variant == "warning" && (
+          <>
+            <Image src={Warning.src} width={16} height={16} alt="" />
+            <div className="text-sm font-normal">{message}</div>
+            <Image src={Close.src} width={20} height={20} alt="" className="cursor-pointer" onClick={() => onClickDismiss(id)} />
+          </>
+        )}
+        {variant == "error" && (
+          <>
+            <Image src={Error.src} width={16} height={16} alt="" />
+            <div className="text-sm font-normal text-accent-red">{message}</div>
+            <Image src={Close.src} width={20} height={20} alt="" className="cursor-pointer" onClick={() => onClickDismiss(id)} />
+          </>
+        )}
+      </div>
+    </SnackbarContent>
+  );
+});
 
-EmsSnackbar.displayName = 'EmsSnackbar';
+EmsSnackbar.displayName = "EmsSnackbar";
 
 const SnackbarUtils = {
   success(msg) {
-    this.toast(msg, { variant: 'success' });
+    this.toast(msg, { variant: "success" });
   },
   warning(msg) {
-    this.toast(msg, { variant: 'warning' });
+    this.toast(msg, { variant: "warning" });
   },
   info(msg) {
-    this.toast(msg, { variant: 'info' });
+    this.toast(msg, { variant: "info" });
   },
   error(msg) {
-    this.toast(msg, { variant: 'error' });
+    this.toast(msg, { variant: "error" });
   },
   toast(msg, props) {
     useSnackbarRef.enqueueSnackbar(msg, {
       ...props,
       anchorOrigin: {
-        vertical: 'top',
-        horizontal: 'center',
+        vertical: "top",
+        horizontal: "center",
       },
     });
   },
