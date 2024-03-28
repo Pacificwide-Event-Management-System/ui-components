26a394c8feda293bdd60de40ba1d6d9ffa0840f7
Hieu Pham <hieupp.work@gmail.com>
2024-03-28 09:57:27 +0700

    Modify style disable for EmsTextField

--- ems-text-field/EmsTextField.tsx
+++ ems-text-field/EmsTextField.tsx
@@ -17,7 +17,7 @@ const EmsTextField = ({
       className={clsx(
         "inline-flex !h-10 w-full rounded-lg border border-neutral-5 !px-3 !py-2 !text-base text-neutral-1 !outline-[0] hover:border-primary-2",
         "active:border-primary-3 active:[box-shadow:none]",
-        "disabled:border-none disabled:!bg-neutral-7 disabled:!text-neutral-3 disabled:hover:border-neutral-5",
+        "disabled:border-neutral-divider disabled:!bg-neutral-7 disabled:!text-neutral-3 disabled:hover:border-neutral-5",
         // 'focus:shadow-[0px_0px_0px_3px_#FC50554D] focus:ring-0',
         textInputClassName
       )}
