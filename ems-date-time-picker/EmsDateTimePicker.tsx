78fe7bef35e1bc50a978f255171ce26d25a32979
levanduc09 <levanduc094@gmail.com>
2024-05-16 11:40:39 +0700

    EmsDateTimePicker

---
+++ ems-date-time-picker/EmsDateTimePicker.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { DatePicker, DatePickerProps } from 'antd';
+import clsx from 'clsx';
+import { HTMLInputTypeAttribute } from 'react';
+import './EmsDateTimePicker.scss';
+
+type Props = {
+  id?: string;
+  label?: string;
+  helperText?: string;
+  placeholder?: string;
+  variant?: string;
+  value?: Date;
+  state?: string;
+  disabled?: boolean;
+  autoComplete?: string;
+  type?: HTMLInputTypeAttribute;
+  onChange?: (value: any) => void;
+  required?: boolean;
+  format?: string;
+  className?: string;
+};
+
+// Custom components
+function EmsDateTimePicker(props: Props & DatePickerProps) {
+  return (
+    <DatePicker
+      {...props}
+      format={'MM/DD/YYYY HH:mm'}
+      className={clsx(
+        'flex !h-10 w-full items-center rounded-lg border border-neutral-5 px-3 !text-base text-neutral-1 !outline-[0] hover:border-primary-2',
+        'active:border-primary-3 active:[box-shadow:none]',
+        'disabled:border-none disabled:!bg-neutral-7 disabled:!text-neutral-3 disabled:hover:border-neutral-5',
+        'focus:shadow-[0px_0px_0px_3px_#FC50554D] focus:ring-0',
+        'overflow-hidden',
+      )}
+      value={props.value}
+      onChange={(value) => {
+        let valid = true;
+        if (value && props.minDate && value < props.minDate) valid = false;
+        if (value && props.maxDate && value > props.maxDate) valid = false;
+        if (!valid) value = null;
+        props.onChange(value);
+      }}
+      autoComplete={props.autoComplete ?? 'off'}
+      required={props.required}
+      placeholder={props.placeholder}
+      disabled={props.disabled}
+    />
+  );
+}
+
+export default EmsDateTimePicker;
