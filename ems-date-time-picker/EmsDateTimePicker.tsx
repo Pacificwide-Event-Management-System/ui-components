63e63b8a5bc32d1bf81bad22b6c29e11b16c09e3
levanduc09 <levanduc094@gmail.com>
2024-05-17 23:39:40 +0700

    Update EmsDateTimePicker

--- ems-date-time-picker/EmsDateTimePicker.tsx
+++ ems-date-time-picker/EmsDateTimePicker.tsx
@@ -1,9 +1,9 @@
-'use client';
-import { DateTimePickerFormat } from '@/libs/constants/dateConstant';
-import { DatePicker, DatePickerProps } from 'antd';
-import clsx from 'clsx';
-import { HTMLInputTypeAttribute } from 'react';
-import './EmsDateTimePicker.scss';
+"use client";
+import { DateTimePickerFormat } from "@/libs/constants/dateConstant";
+import { DatePicker, DatePickerProps } from "antd";
+import clsx from "clsx";
+import { HTMLInputTypeAttribute } from "react";
+import "./EmsDateTimePicker.scss";
 
 type Props = {
   id?: string;
@@ -30,11 +30,11 @@ function EmsDateTimePicker(props: Props & DatePickerProps) {
       format={DateTimePickerFormat}
       needConfirm={false}
       className={clsx(
-        'flex !h-10 w-full items-center rounded-lg border border-neutral-5 px-3 !text-base text-neutral-1 !outline-[0] hover:border-primary-2',
-        'active:border-primary-3 active:[box-shadow:none]',
-        'disabled:border-none disabled:!bg-neutral-7 disabled:!text-neutral-3 disabled:hover:border-neutral-5',
-        'focus:shadow-[0px_0px_0px_3px_#FC50554D] focus:ring-0',
-        'overflow-hidden',
+        "flex !h-10 w-full items-center rounded-lg border border-neutral-5 px-3 !text-base text-neutral-1 !outline-[0] hover:border-primary-2",
+        "active:border-primary-3 active:[box-shadow:none]",
+        "disabled:border-none disabled:!bg-neutral-7 disabled:!text-neutral-3 disabled:hover:border-neutral-5",
+        "focus:shadow-[0px_0px_0px_3px_#FC50554D] focus:ring-0",
+        "overflow-hidden"
       )}
       value={props.value}
       onChange={(value) => {
@@ -44,10 +44,12 @@ function EmsDateTimePicker(props: Props & DatePickerProps) {
         if (!valid) value = null;
         props.onChange(value);
       }}
-      autoComplete={props.autoComplete ?? 'off'}
+      autoComplete={props.autoComplete ?? "off"}
       required={props.required}
       placeholder={props.placeholder}
       disabled={props.disabled}
+      showTime={true}
+      allowClear={true}
     />
   );
 }
