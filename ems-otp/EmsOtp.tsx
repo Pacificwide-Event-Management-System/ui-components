68be03c445983656f254cf80b3b1a57afe130c29
levanduc09 <levanduc094@gmail.com>
2024-06-19 16:16:48 +0700

    Update EmsOtp

--- ems-otp/EmsOtp.tsx
+++ ems-otp/EmsOtp.tsx
@@ -4,11 +4,12 @@ import { Input } from "antd";
 import "./EmsOtp.scss";
 
 type Props = {
+  id: string;
   onChange?: (value: string) => void;
 };
 
 function EmsOtp(props: Props) {
-  return <Input.OTP formatter={(str) => str.toUpperCase()} onChange={props.onChange} />;
+  return <Input.OTP id={props.id} formatter={(str) => str.toUpperCase()} onChange={props.onChange} />;
 }
 
 export default EmsOtp;
