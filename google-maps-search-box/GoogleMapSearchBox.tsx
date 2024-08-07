2017121be76fdbbe97f794f34af2358688c9c0b6
tpthanhan <36980873+tpthanhan@users.noreply.github.com>
2024-08-07 21:12:13 +0700

    consistant google map setting

--- google-maps-search-box/GoogleMapSearchBox.tsx
+++ google-maps-search-box/GoogleMapSearchBox.tsx
@@ -1,20 +1,19 @@
-"use client";
+'use client';
 
-import { Autocomplete, Libraries, useJsApiLoader } from "@react-google-maps/api";
-import React, { useState } from "react";
-import EmsSpiner from "../ems-spiner/EmsSpiner";
+import { GOOGLE_MAP_LIBRARIES_TO_USE } from '@/libs/constants/googleMapsConstant';
+import { Autocomplete, useJsApiLoader } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import EmsSpiner from '../ems-spiner/EmsSpiner';
 //@ts-expect-error
-import GoogleMapsIcon from "@/static/google-maps.svg?component";
-import { InputProps } from "antd";
-import clsx from "clsx";
-import { useTranslations } from "next-intl";
-import EmsTextField from "../ems-text-field/EmsTextField";
-
-const libraries: Libraries = ["places"];
+import GoogleMapsIcon from '@/static/google-maps.svg?component';
+import { InputProps } from 'antd';
+import clsx from 'clsx';
+import { useTranslations } from 'next-intl';
+import EmsTextField from '../ems-text-field/EmsTextField';
 
 type Props = {
   id?: string;
-  state?: "error" | "";
+  state?: 'error' | '';
   textInputClassName?: string;
   onChange?: (place: string) => void;
   onPlaceSelect?: (place: google.maps.places.PlaceResult) => void;
@@ -26,22 +25,21 @@ const GoogleMapSearchBox: React.FC<Props & InputProps> = (props) => {
   const t = useTranslations();
 
   const { isLoaded: isGoogleMapApiLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    // googleMapsApiKey: getGoogleMapsApiKey(),
-    googleMapsApiKey: "AIzaSyBTQCWCIhW2ILIdWY-t3bNPsEHbI2dW1BU",
-    libraries: libraries,
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: GOOGLE_MAP_LIBRARIES_TO_USE,
   });
 
   const [autocomplete, setAutocomplete] = useState<google.maps.places.Autocomplete>();
   const [location, setLocation] = useState<{
     name: string;
   }>({
-    name: (props.value as string) ?? "",
+    name: (props.value as string) ?? '',
   });
 
   const onLoad = (autocomplete: google.maps.places.Autocomplete) => {
     autocomplete.setOptions({
-      fields: ["name", "formatted_address", "place_id"],
+      fields: ['name', 'formatted_address', 'place_id'],
     });
     setAutocomplete(autocomplete);
   };
@@ -68,7 +66,7 @@ const GoogleMapSearchBox: React.FC<Props & InputProps> = (props) => {
       }
       setLocation({ name: place.name });
     } else {
-      console.log("Autocomplete is not loaded yet!");
+      console.log('Autocomplete is not loaded yet!');
     }
   };
 
@@ -76,7 +74,9 @@ const GoogleMapSearchBox: React.FC<Props & InputProps> = (props) => {
     props.helperText ? (
       <label className="mt-2 text-sm text-accent-red">{props.helperText}</label>
     ) : props.showLocationNotSelectedError ? (
-      <label className="mt-2 text-sm text-accent-red">{t("validate_message.please_choose_a_location_for_the_event_on_the_map")}</label>
+      <label className="mt-2 text-sm text-accent-red">
+        {t('validate_message.please_choose_a_location_for_the_event_on_the_map')}
+      </label>
     ) : undefined;
 
   if (isGoogleMapApiLoaded) {
@@ -86,12 +86,16 @@ const GoogleMapSearchBox: React.FC<Props & InputProps> = (props) => {
           <EmsTextField
             {...props}
             suffix={<GoogleMapsIcon />}
-            textInputClassName={clsx(props.disabled && "border-neutral-divider !bg-neutral-7 !text-neutral-3 hover:border-neutral-5", props.textInputClassName)}
+            textInputClassName={clsx(
+              props.disabled &&
+                'border-neutral-divider !bg-neutral-7 !text-neutral-3 hover:border-neutral-5',
+              props.textInputClassName,
+            )}
             value={location.name}
             onChange={onQueryChange}
             id={props.id}
             disabled={!autocomplete || props.disabled}
-            autoComplete={props.autoComplete ?? "off"}
+            autoComplete={props.autoComplete ?? 'off'}
           />
         </Autocomplete>
         {renderHelperText()}
