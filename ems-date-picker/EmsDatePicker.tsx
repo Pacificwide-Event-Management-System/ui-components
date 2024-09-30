10ba7ab2078ca9f4169245ef8c940fca17669820
hieupp <hieupp.work@gmail.com>
2024-09-30 15:35:33 +0700

    Update ems DatePicker

--- ems-date-picker/EmsDatePicker.tsx
+++ ems-date-picker/EmsDatePicker.tsx
@@ -1,10 +1,9 @@
 'use client';
-import { DatePickerFormat } from '@/libs/constants/dateConstant';
-import { DatePicker } from 'antd';
+import { DateTimePickerFormat } from '@/libs/constants/dateConstant';
+import { DatePicker, DatePickerProps } from 'antd';
 import clsx from 'clsx';
-import dayjs from 'dayjs';
 import { HTMLInputTypeAttribute } from 'react';
-import './EmsDatePicker.scss';
+import './EmsDateTimePicker.scss';
 
 type Props = {
   id?: string;
@@ -17,37 +16,48 @@ type Props = {
   disabled?: boolean;
   autoComplete?: string;
   type?: HTMLInputTypeAttribute;
-  onChange?: (value: Date) => void;
+  onChange?: (value: any) => void;
   required?: boolean;
   format?: string;
   className?: string;
-  minDate?: Date;
-  maxDate?: Date;
+  minDate?: any;
+  maxDate?: any;
+  defaultValue?: any;
 };
 
 // Custom components
-function EmsDatePicker(props: Props) {
+function EmsDateTimePicker(
+  props: Props & Omit<DatePickerProps, 'minDate' | 'maxDate' | 'defaultValue'>,
+) {
   return (
     <DatePicker
-      format={DatePickerFormat}
-      allowClear={false}
+      {...props}
+      format={props.format ? props.format : DateTimePickerFormat}
+      needConfirm={false}
       className={clsx(
         'flex !h-10 w-full items-center rounded-lg border border-neutral-5 px-3 !text-base text-neutral-1 !outline-[0] hover:border-primary-2',
         'active:border-primary-3 active:[box-shadow:none]',
-        'disabled:border-none disabled:!bg-neutral-7 disabled:!text-neutral-3 disabled:hover:border-neutral-5',
+        props.disabled &&
+          'border-neutral-divider !bg-neutral-7 !text-neutral-3 hover:border-neutral-5',
         'focus:shadow-[0px_0px_0px_3px_#FC50554D] focus:ring-0',
         'overflow-hidden',
       )}
       value={props.value}
-      onChange={(value) => props.onChange(value)}
+      onChange={(value) => {
+        let valid = true;
+        if (value && props.minDate && value < props.minDate) valid = false;
+        if (value && props.maxDate && value > props.maxDate) valid = false;
+        if (!valid) value = null;
+        props.onChange(value);
+      }}
       autoComplete={props.autoComplete ?? 'off'}
       required={props.required}
       placeholder={props.placeholder}
       disabled={props.disabled}
-      minDate={props.minDate ? dayjs(props.minDate) : undefined}
-      maxDate={props.maxDate ? dayjs(props.maxDate) : undefined}
+      showTime={true}
+      allowClear={true}
     />
   );
 }
 
-export default EmsDatePicker;
+export default EmsDateTimePicker;
